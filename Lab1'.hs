eca9e9903f14946b0a151774227b4ac0890bc402
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-11-03 20:58:03 +0100

    My solutions + some indentation fixes.

--- Lab1'.hs
+++ Lab1'.hs
@@ -2,30 +2,30 @@
 
 -- Calculate n^k recursively.
 power :: Integer -> Integer -> Integer
-power n k | k < 0	= error "Exponent less than 0"
+power n k | k < 0 = error "power: negative argument"
 power n 0 = 1
 power n k = n * power n (k-1)
 
 -- Part 1
 
--- 'power n k' takes k+1 steps for all k >= 0
+-- It takes k + 1 steps.
 
 -- Part 2
 
--- Calculate n^k by calling the product of a list with n repeated times.
+-- Calculate n^k by calling calculating the product of a list with n repeated
+-- times.
 power1 :: Integer -> Integer -> Integer
-power1 n k | k >= 0 = product [n | x <- [1..k]]
-           | otherwise = error "Exponent less than 0"
+power1 n k | k >= 0    = product [n | x <- [1..k]]
+           | otherwise = error "power with negative exponent not allowed"
 
 -- Part 3
 
 -- Calculate n^k by splitting k in half when it is even.
 power2 :: Integer -> Integer -> Integer
 power2 n 0             = 1
-power2 n k | k < 0     = error "Exponent less than 0"
-           | even k    = power2 (n * n) (div k 2)
+power2 n k | k < 0     = error "power with negative exponent not allowed"
+           | even k    = power2 (n*n) (div k 2)
            | otherwise = n * power2 n (k-1)
-
 -- Part 4
 
 -- A
@@ -36,7 +36,7 @@ power2 n k | k < 0     = error "Exponent less than 0"
   * k < 0 - to ensure that an error is thrown with negative exponents
   * k > 0 - to ensure that positive exponents are handled correctly
   * even k - since one of the function treats odd and even numbers
-  differently
+    differently
   * odd k - since one of the function treats odd and even numbers differently
   * n == 0
   * n < 0
