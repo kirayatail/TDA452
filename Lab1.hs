1960edf6a74044e68b567055f8016faa1d3c40b7
Max Witt <kirayatail@gmail.com>
2014-11-04 20:49:01 +0100

    final version of Lab1

--- Lab1.hs
+++ Lab1.hs
@@ -1,8 +1,10 @@
 -- Lab Assignment 1
+import Data.List
+import Test.QuickCheck
 
 -- Calculate n^k recursively.
 power :: Integer -> Integer -> Integer
-power n k | k < 0	= error "Exponent less than 0"
+power n k | k < 0	= error "power: negative exponent"
 power n 0 = 1
 power n k = n * power n (k-1)
 
@@ -14,15 +16,15 @@ power n k = n * power n (k-1)
 
 -- Calculate n^k by calling the product of a list with n repeated times.
 power1 :: Integer -> Integer -> Integer
-power1 n k | k >= 0 = product [n | x <- [1..k]]
-           | otherwise = error "Exponent less than 0"
+power1 n k | k >= 0 = product (genericReplicate k n)
+           | otherwise = error "power1: negative exponent"
 
 -- Part 3
 
 -- Calculate n^k by splitting k in half when it is even.
 power2 :: Integer -> Integer -> Integer
 power2 n 0             = 1
-power2 n k | k < 0     = error "Exponent less than 0"
+power2 n k | k < 0     = error "power2: negative exponent"
            | even k    = power2 (n * n) (div k 2)
            | otherwise = n * power2 n (k-1)
 
@@ -32,17 +34,41 @@ power2 n k | k < 0     = error "Exponent less than 0"
 
 {-
   We believe the following test cases are important:
-  * k == 0 - to ensure that the base case is handled correctly
-  * k < 0 - to ensure that an error is thrown with negative exponents
+  * k == 0 - to ensure that the base case is handled correctly,
+    result should always be 1
+  * k < 0 - Avoid testing this property, as it's not defined for the function
   * k > 0 - to ensure that positive exponents are handled correctly
   * even k - since one of the function treats odd and even numbers
-  differently
+    differently
   * odd k - since one of the function treats odd and even numbers differently
-  * n == 0
-  * n < 0
+  * n == 0 - should be 0 for all k except k == 0 where it should be 1
+  * n < 0 - should handle negative n
   * n > 0
 -}
 
+
 -- B
 
+-- Property that checks that power, power1 and power2 produce equal results.
+-- Negative values for k are avoided by only using the absolute value.
+prop_powers :: Integer -> Integer -> Bool
+prop_powers n k = let k' = abs k in 
+                  power2 n k' == power n k' &&
+                  power1 n k' == power n k'
+
+
 -- C
+
+--  Cartesian product for n X k as mentioned in part A.
+prop_powersAll :: Integer -> Integer -> Bool
+prop_powersAll n k = let ns = [n, 0, (-n)] in
+                     let ks = [0, (abs k), (1 + abs k)] in
+                     and [prop_powers n' k' | n' <- ns, k' <- ks]
+
+
+-- D
+
+{-
+  We avoided getting errors from our power functions since we decided not
+  to test negative k values in prop_powers by using the absolute value.
+-}
