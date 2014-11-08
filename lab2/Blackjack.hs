bcd588cc6fb120a7ad939b5da0e900969f9bfae0
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-11-08 15:04:48 +0100

    Version of value that only need to traverse the hand once.

--- lab2/Blackjack.hs
+++ lab2/Blackjack.hs
@@ -1,3 +1,7 @@
+--  Lab 2 - A  Simple Black Jack Variant
+
+--Part 2
+
 -- Hand execution of size hand2
 {-
 size hand2
@@ -16,6 +20,19 @@ hand3 = Add (Card Ace Spades) (Add (Card Ace Hearts) (Add (Card (Numeric 3) Hear
 
 empty :: Hand
 empty = Empty
+
+value :: Hand -> Integer
+value h = let (val, aces) = value' h in
+          let bigVal = val + aces * 11 in
+          if bigVal < 21 then bigVal
+          else val + aces
+
+
+value' :: Hand ->  (Integer, Integer)
+value' Empty = (0, 0)
+value' (Add (Card Ace _) n) = tupleSum (0, 1) (value' n)
+value' (Add (Card (Numeric num) _) n) = tupleSum (num, 0) (value' n)
+value' (Add _ n) = tupleSum (10, 0) (value' n)
 
 tupleSum:: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
 tupleSum (a, b) (a', b') = (a + a', b + b')
