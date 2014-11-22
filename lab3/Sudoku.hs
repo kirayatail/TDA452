8c6e7e7032e481a988e65f6531877ae728903da5
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-11-22 21:08:35 +0100

    Some type fixes to fields''.

--- lab3/Sudoku.hs
+++ lab3/Sudoku.hs
@@ -79,7 +79,7 @@ isOkayBlock b = length (nub (Nothing:b)) == 10 - length (filter (== Nothing) b)
 
 -- Create a list of all blocks of a sudoku (rows, cols, 3x3-fields)
 blocks :: Sudoku -> [Block]
-blocks (Sudoku rows) = rows ++ transpose rows ++ fields rows
+blocks (Sudoku rows) = rows ++ transpose rows ++ fields'' rows
 
 -- Apply fields' to three transposed rows at a time,
 -- merge the result to a list of blocks.
@@ -94,7 +94,8 @@ fields' :: [Block] -> [Block]
 fields' []     = []
 fields' blocks = concat (take 3 blocks) : fields' (drop 3 blocks)
 
-fields'' (Sudoku rows) = mergeRows $ concat $ transpose $ map splitRow rows
+fields'' :: [Block] -> [Block]
+fields'' rows = mergeRows $ concat $ transpose $ map splitRow rows
   where
     splitRow :: [a] -> [[a]]
     splitRow [] = []
