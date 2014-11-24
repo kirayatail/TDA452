6cb9ed69b978827a46ba9de2ab68a1a8e5110272
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-11-24 17:21:20 +0100

    Removed example sudoku.

--- lab3/Sudoku.hs
+++ lab3/Sudoku.hs
@@ -101,19 +101,3 @@ prop_SudokuCorrectBlocks sudo = length (blocks sudo) == (3*9) &&
 -- Check that a full sudoku is valid, all blocks should be OK
 isOkay :: Sudoku -> Bool
 isOkay sudo = and [isOkayBlock b | b <- blocks sudo]
-
-
--- Example sudoku:
-example :: Sudoku
-example =
-  Sudoku
-  [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
-  , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
-  , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
-  , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
-  , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
-  , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
-  , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
-  , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
-  , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
-  ]
