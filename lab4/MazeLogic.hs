61e1c33a9af2cee259022d3f839e047e87a9614c
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-12-11 11:49:57 +0100

    Working fullMaze.

--- lab4/MazeLogic.hs
+++ lab4/MazeLogic.hs
@@ -3,16 +3,22 @@
 module MazeLogic where
 
 data Maze = Maze { vertical :: [[Wall]], horizontal :: [[Wall]]}
+  deriving ( Show, Eq )
 
 data Wall = Open | Blocked
+  deriving ( Show, Eq )
 
 data Direction = Up | Down | Left | Right
+  deriving ( Show, Eq )
 
 type Pos = (Int,Int)
 
 emptyMaze, fullMaze :: Int -> Int -> Maze
 emptyMaze = undefined
-fullMaze = undefined
+-- fullMaze x y = Maze {vertical = (replicate x + 1 $ wallList x) horizontal = (replicate y + 1 $ wallList y)}
+fullMaze x y = Maze {vertical = replicate (x + 1) $ wallList y, horizontal = replicate (y + 1) $ wallList x}
+  where
+    wallList n = replicate n Blocked
 
 addWall, removeWall :: Maze -> Pos -> Direction
 addWall = undefined
