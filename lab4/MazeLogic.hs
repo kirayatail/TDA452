810a130be27d918b8f433d5d1b8ceb5e1cd40e50
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-12-11 12:39:33 +0100

    Implementation of showMaze.

--- lab4/MazeLogic.hs
+++ lab4/MazeLogic.hs
@@ -2,6 +2,8 @@
 
 module MazeLogic where
 
+import Data.List
+
 data Maze = Maze { vertical :: [[Wall]], horizontal :: [[Wall]]}
   deriving ( Show, Eq )
 
@@ -12,6 +14,25 @@ data Direction = Up | Down | Left | Right
   deriving ( Show, Eq )
 
 type Pos = (Int,Int)
+
+verticals, horizontals :: Maze -> [[Wall]]
+verticals (Maze v _) = v
+horizontals (Maze _ h) = h
+
+showMaze :: Maze -> String
+showMaze m = showHorizontal vs hs
+  where
+    vs = transpose $ verticals m
+    hs = horizontals m
+    showHorizontal [] []        = []
+    showHorizontal vs' (h:hs') = concatMap showHorizontalWall h ++ "\n" ++showVertical vs' hs'
+    showVertical [] []        = []
+    showVertical (v:vs') hs' = concatMap showVerticalWall v ++ "\n" ++ showHorizontal vs' hs'
+    showVerticalWall Open = "   "
+    showVerticalWall Blocked = "|  "
+    showHorizonalWall Open = "   "
+    showHorizontalWall Blocked = " ——"
+
 
 emptyMaze, fullMaze :: Int -> Int -> Maze
 emptyMaze = undefined
