8ad301484b6284dce226f8c35204bfefc24f8437
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-12-13 16:46:52 +0100

    Comments for MazeGenerator.

--- lab4/MazeGenerator.hs
+++ lab4/MazeGenerator.hs
@@ -3,14 +3,17 @@ module MazeGenerator where
 import MazeLogic
 import System.Random
 
+-- Prototype for generating a maze with pluggable algorithm.
 protoGenerateMaze :: (StdGen -> Int -> Int -> Maze) -> Int -> Int -> IO ()
 protoGenerateMaze f w h = do
   g <- newStdGen
   let l = f g w h
   putStr $ showMaze l
 
+-- Generate using recursive backtracking.
 generateRBMaze :: Int -> Int -> IO ()
 generateRBMaze = protoGenerateMaze recursiveBacktracker
 
+-- Generate using Prim's algorithm.
 generatePrimsMaze :: Int -> Int -> IO ()
 generatePrimsMaze = protoGenerateMaze prims
